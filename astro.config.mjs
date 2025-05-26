8cc47e60aa3c2a5c7d1b3d552d5258c209c9407f
Jonny Woodhead <jonny@jonnys-mbp.home>
2025-05-27 00:23:19 +0100

    Enable SSR mode in Astro config for API routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
-
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [react(), tailwind()],
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  })
 });
